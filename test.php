b78e9daa0160dc93e978937b6a1e4d5fd3c2ad93
SlyCatt <glukshpilz@yandex.ru>
2020-08-18 16:47:21 +0300

    Add files via upload

--- test.php
+++ test.php
@@ -1,7 +1,44 @@
 <?php
-    function config($req, $default=NULL)
+    echo config("db.user", "./testfile.txt")
+
+    function config($req, $filename, $default=NULL)
     {
-        $req;
+        if(strripos($req, ".")!==false
+        {
+            $address = explode(".", trim($req));
+        }
+        else $address = array($req);
+
+        $filecontent = file_get_contents($filename);
+        $startingpoint = 0;
+        foreach($address as &$point)
+        {
+            $reqpos = strpos($filecontent, $point, $startingpoint);
+            if($reqpos === false)
+            {
+                if($default!=NULL)
+                {
+                    return $default;
+                }
+                else return false;
+            }
+            else
+            {
+                $resalutstring = substr($filecontent, $reqpos, strpos($filecontent, "\n", $reqpos) - $reqpos);
+                if(strpos($resalutstring, "[", $reqpos)!==false) 
+                {
+                    $startingpoint = strpos($filecontent, "[", $reqpos);
+                    continue;
+                }
+                else
+                {
+                    $resalut = trim(substr($resalutstring, strpos($filecontent, "=>", $reqpos) + 2, strpos($filecontent, "\n", $reqpos)-strpos($filecontent, "=>", $reqpos) + 2));
+                    return str_replace("\"", "", $resalut);
+                }
+                
+                
+            }
+        }
     }
 
 ?>
