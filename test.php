2451ab4891c3cf3d2d3da00044081fbce25b68ca
SlyCatt <glukshpilz@yandex.ru>
2020-08-18 23:12:45 +0300

    Add files via upload

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
-    echo config("db.user", "./testfile.txt")
+    echo config("app.services.resizer.prefer_format", "./testfile.txt", "web");
 
     function config($req, $filename, $default=NULL)
     {
-        if(strripos($req, ".")!==false
+        if(strripos($req, ".")!==false)
         {
             $address = explode(".", trim($req));
         }
@@ -13,27 +13,30 @@
         $startingpoint = 0;
         foreach($address as &$point)
         {
-            $reqpos = strpos($filecontent, $point, $startingpoint);
+            $reqpos = strpos($filecontent, "\"".$point."\"", $startingpoint);
             if($reqpos === false)
             {
                 if($default!=NULL)
                 {
                     return $default;
                 }
-                else return false;
+                else{
+                throw new Exception('Something went wrong');
+                return false;
+                }
             }
             else
             {
                 $resalutstring = substr($filecontent, $reqpos, strpos($filecontent, "\n", $reqpos) - $reqpos);
-                if(strpos($resalutstring, "[", $reqpos)!==false) 
+                if(strpos($resalutstring, "[")!==false) 
                 {
                     $startingpoint = strpos($filecontent, "[", $reqpos);
                     continue;
                 }
                 else
                 {
-                    $resalut = trim(substr($resalutstring, strpos($filecontent, "=>", $reqpos) + 2, strpos($filecontent, "\n", $reqpos)-strpos($filecontent, "=>", $reqpos) + 2));
-                    return str_replace("\"", "", $resalut);
+                    $resalut = trim(substr($filecontent, strpos($filecontent, "=>", $reqpos) + 2, strpos($filecontent, "\n", $reqpos)-strpos($filecontent, "=>", $reqpos) + 2));
+                    return str_replace(",", "", str_replace("\"", "", $resalut));
                 }
                 
                 
